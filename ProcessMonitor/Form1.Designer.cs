82b7a8a9ba2d24032200f42054f52b4c1cf7038b
Trung Trần <68211192+trung501@users.noreply.github.com>
2024-08-26 00:54:48 +0700

    update

--- ProcessMonitor/Form1.Designer.cs
+++ ProcessMonitor/Form1.Designer.cs
@@ -4,110 +4,124 @@
     {
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ProcessName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LastActivity = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.labelHeader = new System.Windows.Forms.Label();
-            this.labelElapsedTime = new System.Windows.Forms.Label();
-            this.buttonStartStop = new System.Windows.Forms.Button(); // Add this line
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            dataGridView1 = new DataGridView();
+            PID = new DataGridViewTextBoxColumn();
+            ProcessName = new DataGridViewTextBoxColumn();
+            Status = new DataGridViewTextBoxColumn();
+            LastActivity = new DataGridViewTextBoxColumn();
+            labelHeader = new Label();
+            labelElapsedTime = new Label();
+            buttonStartStop = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.PID,
-            this.ProcessName,
-            this.Status,
-            this.LastActivity});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 100); // Adjusted to make space for the button
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 338);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.None;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { PID, ProcessName, Status, LastActivity });
+            dataGridView1.Location = new Point(12, 100);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(776, 338);
+            dataGridView1.TabIndex = 0;
             // 
             // PID
             // 
-            this.PID.HeaderText = "PID";
-            this.PID.MinimumWidth = 6;
-            this.PID.Name = "PID";
-            this.PID.Width = 80;
-            this.PID.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            PID.DefaultCellStyle = dataGridViewCellStyle2;
+            PID.HeaderText = "PID";
+            PID.MinimumWidth = 6;
+            PID.Name = "PID";
+            PID.Width = 80;
             // 
             // ProcessName
             // 
-            this.ProcessName.HeaderText = "Name";
-            this.ProcessName.MinimumWidth = 6;
-            this.ProcessName.Name = "ProcessName";
-            this.ProcessName.Width = 350;
-            this.ProcessName.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            ProcessName.DefaultCellStyle = dataGridViewCellStyle3;
+            ProcessName.HeaderText = "Name";
+            ProcessName.MinimumWidth = 6;
+            ProcessName.Name = "ProcessName";
+            ProcessName.Width = 350;
             // 
             // Status
             // 
-            this.Status.HeaderText = "Status";
-            this.Status.MinimumWidth = 6;
-            this.Status.Name = "Status";
-            this.Status.Width = 150;
-            this.Status.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            Status.DefaultCellStyle = dataGridViewCellStyle4;
+            Status.HeaderText = "Status";
+            Status.MinimumWidth = 6;
+            Status.Name = "Status";
+            Status.Width = 150;
             // 
             // LastActivity
             // 
-            this.LastActivity.HeaderText = "Last Activity";
-            this.LastActivity.MinimumWidth = 6;
-            this.LastActivity.Name = "LastActivity";
-            this.LastActivity.Width = 176;
-            this.LastActivity.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            LastActivity.DefaultCellStyle = dataGridViewCellStyle5;
+            LastActivity.HeaderText = "Last Activity";
+            LastActivity.MinimumWidth = 6;
+            LastActivity.Name = "LastActivity";
+            LastActivity.Width = 176;
             // 
             // labelHeader
             // 
-            this.labelHeader.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelHeader.Location = new System.Drawing.Point(12, 9);
-            this.labelHeader.Name = "labelHeader";
-            this.labelHeader.Size = new System.Drawing.Size(576, 40);
-            this.labelHeader.TabIndex = 1;
-            this.labelHeader.Text = "Process Monitor";
-            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            labelHeader.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            labelHeader.ForeColor = Color.Firebrick;
+            labelHeader.Location = new Point(12, 9);
+            labelHeader.Name = "labelHeader";
+            labelHeader.Size = new Size(576, 40);
+            labelHeader.TabIndex = 1;
+            labelHeader.Text = "Process Monitor";
+            labelHeader.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // labelElapsedTime
             // 
-            this.labelElapsedTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelElapsedTime.Location = new System.Drawing.Point(594, 9);
-            this.labelElapsedTime.Name = "labelElapsedTime";
-            this.labelElapsedTime.Size = new System.Drawing.Size(194, 40);
-            this.labelElapsedTime.TabIndex = 2;
-            this.labelElapsedTime.Text = "Elapsed Time: 0s";
-            this.labelElapsedTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            labelElapsedTime.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelElapsedTime.Location = new Point(594, 9);
+            labelElapsedTime.Name = "labelElapsedTime";
+            labelElapsedTime.Size = new Size(194, 40);
+            labelElapsedTime.TabIndex = 2;
+            labelElapsedTime.Text = "Elapsed Time: 0s";
+            labelElapsedTime.TextAlign = ContentAlignment.MiddleRight;
             // 
             // buttonStartStop
             // 
-            this.buttonStartStop.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonStartStop.Location = new System.Drawing.Point(12, 55);
-            this.buttonStartStop.Name = "buttonStartStop";
-            this.buttonStartStop.Size = new System.Drawing.Size(100, 30);
-            this.buttonStartStop.TabIndex = 3;
-            this.buttonStartStop.Text = "Start";
-            this.buttonStartStop.UseVisualStyleBackColor = true;
-            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            buttonStartStop.BackColor = SystemColors.Info;
+            buttonStartStop.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonStartStop.ForeColor = SystemColors.Desktop;
+            buttonStartStop.Location = new Point(12, 55);
+            buttonStartStop.Name = "buttonStartStop";
+            buttonStartStop.Size = new Size(100, 30);
+            buttonStartStop.TabIndex = 3;
+            buttonStartStop.Text = "Start";
+            buttonStartStop.UseVisualStyleBackColor = false;
+            buttonStartStop.Click += buttonStartStop_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonStartStop);
-            this.Controls.Add(this.labelElapsedTime);
-            this.Controls.Add(this.labelHeader);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
-            this.Text = "Process Monitor";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonStartStop);
+            Controls.Add(labelElapsedTime);
+            Controls.Add(labelHeader);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Process Monitor";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         private System.Windows.Forms.DataGridView dataGridView1;
